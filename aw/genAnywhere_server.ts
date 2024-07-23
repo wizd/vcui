3c20d03ce2831405d9b61186e1877ff605b13d8e
Wizard <rcvbuf@gmail.com>
2024-07-23 13:09:53 +0800

    feat: add genAnyhwere

---
+++ aw/genAnywhere_server.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { streamText } from 'ai';
+import { createOpenAI } from '@ai-sdk/openai';
+import { createStreamableValue } from 'ai/rsc';
+
+export async function generate(input: string) {
+  const stream = createStreamableValue('');
+
+  (async () => {
+    
+    const openai = createOpenAI({
+        baseURL: 'http://192.168.3.56:5011/v1',
+        apiKey: 'aaa'
+    });
+
+    const { textStream } = await streamText({
+      model: openai('gpt-4'),
+      prompt: input,
+    });
+
+    for await (const delta of textStream) {
+      stream.update(delta);
+    }
+
+    stream.done();
+  })();
+
+  return { output: stream.value };
+}
